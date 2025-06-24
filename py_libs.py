e896bda5ee6174c52be5a773367987aadc2170d4
Shivansh Tiwari <shivanshtiwari6849@gmail.com>
2025-06-24 13:48:02 +0530

    Add PyTorch section to library overview

--- py_libs.py
+++ py_libs.py
@@ -2,3 +2,8 @@
 
 # It provides many inbuilt methods for groping, combining and filtering data. Keras is a very popular Machine Learning library for Python. It is a high-level neural networks API capable of running on top of TensorFlow, CNTK, or Theano. It can run seamlessly on both CPU and GPU. Keras makes it really for ML beginners to build and design a Neural Network. One of the best things about Keras is that it allows for easy and fast prototyping.
 
+# ---------------------------------------------------------------------------------------------------------
+
+# PYTORCH
+
+# PyTorch is a popular open-source Machine Learning library for Python based on Torch, which is an open-source Machine Learning library that is implemented in C with a wrapper in Lua. It has an extensive choice of tools and libraries that support Computer Vision, Natural Language Processing (NLP), and many more ML programs. It allows developers to perform computations on Tensors with GPU acceleration and also helps in creating computational graphs.
