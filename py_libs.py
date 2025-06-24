defd665bfe4f28281bb1121f22cd8e5a1df989bf
Shivansh Tiwari <shivanshtiwari6849@gmail.com>
2025-06-24 13:42:58 +0530

    Add notes on supervised learning classification and introduce Keras library overview
    - Created a new notes.txt file detailing the classification of supervised learning into classification and regression, including definitions, examples, and usage.
    - Added explanations for bias and variance in the context of model training.
    - Introduced a new py_libs.py file with an overview of the Keras library, highlighting its capabilities for building neural networks and its ease of use for beginners.

---
+++ py_libs.py
@@ -0,0 +1,4 @@
+KERAS
+
+It provides many inbuilt methods for groping, combining and filtering data. Keras is a very popular Machine Learning library for Python. It is a high-level neural networks API capable of running on top of TensorFlow, CNTK, or Theano. It can run seamlessly on both CPU and GPU. Keras makes it really for ML beginners to build and design a Neural Network. One of the best things about Keras is that it allows for easy and fast prototyping.
+
